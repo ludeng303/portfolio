fc611715a3154abf60c936ff55bcaedecf452b77
sxiaolu86 <34197113+sxiaolu86@users.noreply.github.com>
2019-10-27 18:59:42 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
   function showAbout() {
     $('#js-sec-about').show();
     $('#js-sec-project01').hide();
+    $('#js-sec-project02').hide();
+    $('#js-sec-project03').hide();
     document.getElementById("js-p-about").style.color = "#cc992c";
     document.getElementById("js-p-projects").style.color = "#EDF1F5";
   }
@@ -8,6 +10,8 @@
   
   function showProjects() {
     $('#js-sec-project01').show();
+    $('#js-sec-project02').show();
+    $('#js-sec-project03').show();
     $('#js-sec-about').hide();
     document.getElementById("js-p-projects").style.color = "#cc992c";
     document.getElementById("js-p-about").style.color = "#EDF1F5";
